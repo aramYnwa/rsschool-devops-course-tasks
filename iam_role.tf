f07fbf22ba4e18516f92f170dc95c4729efbc1fd
akh <akh@hubspot.com>
2024-10-06 15:01:47 +0200

    Variable cannot be used in terraform configuration block

--- iam_role.tf
+++ iam_role.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "rsschool-terraform-state"
     key     = "state/terraform.tfstate"
-    region  = var.aws_region
+    region  = "eu-central-1"
     encrypt = true             
   }
 }
