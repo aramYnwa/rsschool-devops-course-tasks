940fc419415694148427855a3082bf576f6e9b05
akh <akh@hubspot.com>
2024-10-06 14:46:25 +0200

    Move aws region into variable

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "rsschool-terraform-state" # Replace with your bucket name
-    key     = "state/terraform.tfstate"  # File path to store state in the bucket
-    region  = "eu-central-1"             # Replace with your bucket's region
-    encrypt = true                       # Encrypt state file at rest
+    bucket  = "rsschool-terraform-state"
+    key     = "state/terraform.tfstate"
+    region  = var.aws_region
+    encrypt = true             
   }
 }
