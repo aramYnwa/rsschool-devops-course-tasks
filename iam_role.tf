4c86467520fd5cc2793da458a35502d61a3b32f9
aram <aramaiskh@gmail.com>
2024-10-09 20:11:36 +0000

    Modify policy attachments to use for_each

--- iam_role.tf
+++ iam_role.tf
@@ -31,38 +31,17 @@ resource "aws_iam_role" "github_actions_role" {
   })
 }
 
-# Attach Managed Policies to the Role
-resource "aws_iam_role_policy_attachment" "attach_ec2" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
+resource "aws_iam_role_policy_attachment" "policy_attachments" {
+  for_each = {
+    ec2       = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    route53   = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+    s3        = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    iam       = "arn:aws:iam::aws:policy/IAMFullAccess"
+    vpc       = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+    sqs       = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+    eventbridge = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+  }
 
-resource "aws_iam_role_policy_attachment" "attach_route53" {
   role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "attach_iam" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "attach_vpc" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "attach_sqs" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "attach_eventbridge" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+  policy_arn = each.value
 }
